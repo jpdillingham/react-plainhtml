15c8a66bbba515cd836c014e06c485399a646109
JP Dillingham <jp.dillingham@mavtechglobal.com>
2017-11-26 18:32:41 -0600

    Split game components out into game.js.  Added index.js to allow serving with node

---
+++ index.js
@@ -0,0 +1,11 @@
+// sets up a simple static file webserver with node
+// this is necessary because certain functionality is blocked
+// when serving from file://
+var express = require('express');
+var app = express();
+
+app.use('/', express.static(__dirname));
+
+app.listen(3000, function() {
+    console.log('Development server started on port 3000.');
+});
