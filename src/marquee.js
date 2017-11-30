c839b7c60213906e968e38a6e81cc67dd32ecf5b
JP Dillingham <jpdillingham@gmail.com>
2017-11-29 19:27:26 -0600

    Added greeting property to marquee

--- src/marquee.js
+++ src/marquee.js
@@ -1,12 +1,10 @@
 class Marquee extends React.Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
-            <h1>Simple Reactjs app using plain HTML</h1>
+            <h1>{this.props.greeting}</h1>
         );
     }
 }
-
-ReactDOM.render(
-    <Marquee />,
-    document.getElementById('title')
-);
