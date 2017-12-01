10615f6348ae31d35b26c8ba7090c6b96d4646f8
JP Dillingham <jpdillingham@gmail.com>
2017-11-30 21:57:31 -0600

    Added MarqueeControl

--- src/marquee.js
+++ src/marquee.js
@@ -2,9 +2,28 @@ class Marquee extends React.Component {
     constructor(props) {
         super(props);
     }
+    
     render() {
         return (
-            <h1>{this.props.greeting}</h1>
+            <h1 id={this.props.id}>{this.props.greeting}</h1>
         );
+    }
+}
+
+class MarqueeControl extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    extendGreeting(extension) {
+        let control = document.getElementById(this.props.boundMarquee);
+
+        control.innerText = control.innerText + extension;
+    }
+
+    render() {
+        return (
+            <button onClick={() => this.extendGreeting('a')}>Clickme</button>
+        )
     }
 }
