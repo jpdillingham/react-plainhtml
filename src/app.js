622c700d9fd0b8a52fa34a89b741160705ebd274
JP Dillingham <jpdillingham@gmail.com>
2017-12-04 21:09:09 -0600

    Added crypto component

--- src/app.js
+++ src/app.js
@@ -26,7 +26,13 @@ class App extends React.Component {
                 <NavigationButton id='about' content={<About/>} handler={this.navigate} activeButton={this.state.activeButton}>
                     About
                 </NavigationButton>
-                
+                <NavigationButton id='btc' content={<Crypto symbol='BTC'/>} handler={this.navigate} activeButton={this.state.activeButton}>
+                    BTC
+                </NavigationButton>
+                <NavigationButton id='xrp' content={<Crypto symbol='XRP'/>} handler={this.navigate} activeButton={this.state.activeButton}>
+                    XRP
+                </NavigationButton>
+
                 <ContentFrame>
                     {this.state.currentContent}
                 </ContentFrame>
---
+++ src/content/crypto.js
@@ -0,0 +1,38 @@
+class Crypto extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            usd: 0,
+            eur: 0,
+        }
+    }
+
+    componentWillMount() {
+        this.fetchData(this.props.symbol);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.fetchData(nextProps.symbol);
+    }
+
+    fetchData(symbol) {
+        let url = 'https://min-api.cryptocompare.com/data/price?fsym=' + symbol + '&tsyms=USD,EUR';
+
+        axios.get(url)
+        .then((response) => {
+            this.setState({ usd: response.data.USD, eur: response.data.EUR });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                {this.props.symbol} {this.state.usd} {this.state.eur}
+            </div>
+        );
+    }
+}
