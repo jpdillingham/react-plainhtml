08c3e2fdbb858600b740a1e258dfefdf6ecfd16a
JP Dillingham <jpdillingham@gmail.com>
2017-12-20 20:01:44 -0600

    Removed tictactoe components

--- src/components/content/Abc.js
+++ src/components/content/Abc.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Marquee from '../Marquee';
-import Game from '../tictactoe/Game';
 
 class ABCContent extends Component {
     render() {
@@ -8,7 +7,6 @@ class ABCContent extends Component {
             <div>
                 ABC
                 <Marquee id="first" greeting="Tic Tac Toe!"/>
-                <Game />
                 <Marquee id="second" greeting="This is another greeting!"/>
             </div>
         );
